99a493406755bd26bc8e56788ae5f671961d955a
Mohamed Fasil <fazil.mes53@gmail.com>
2020-06-17 14:38:39 +0530

    Initial commit with spring dependencies for web, lombok and H2 DB

---
+++ src/main/java/com/learn/springbootjavastreams/SpringbootJavastreamsApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springbootjavastreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJavastreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJavastreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/springbootjavastreams/SpringbootJavastreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springbootjavastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJavastreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

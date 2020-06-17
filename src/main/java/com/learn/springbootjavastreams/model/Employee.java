fb161f55125e91a48d257c7d1471e8334e568753
Mohamed Fasil <fazil.mes53@gmail.com>
2020-06-17 15:28:21 +0530

    Added JPA dependency, created model class with lombok annonations

---
+++ src/main/java/com/learn/springbootjavastreams/model/Employee.java
@@ -0,0 +1,23 @@
+package com.learn.springbootjavastreams.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "employeeDetails")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int employeeId;
+    @Column(name = "employeeName")
+    private String employeeName;
+    @Column(name = "employeeSalary")
+    private BigDecimal employeeSalary;
+}
